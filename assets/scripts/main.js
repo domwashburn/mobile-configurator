0899da96950d357a17ee725ee029448a15903585
Dominick Washburn <domwashburn@gmail.com>
2015-05-02 13:27:40 -0500

    added 3d flip to the card, still need to figure out the CSS backface property and and how to trigger it with a click.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,14 @@
+//using TweenLite.set() takes care of all vendor-prefixes
+TweenLite.set(".card__holder", {perspective:1500});
+TweenLite.set(".card", {transformStyle:"preserve-3d"});
+TweenLite.set(".card--back", {rotationY:-180});
+TweenLite.set([".back", ".front"], {backfaceVisibility:"hidden"});
 
+$(".button--flip.is-front").hover(
+  function() {
+    TweenLite.to($(".card__holder").find(".card"), 1.2, {rotationY:180, ease:Back.easeOut});
+  },
+  function() {
+    TweenLite.to($(".card__holder").find(".card"), 1.2, {rotationY:0, ease:Back.easeOut});  
+  }
+);

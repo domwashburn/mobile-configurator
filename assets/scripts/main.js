fe302a7c17d8e1790f07dba8b5f38814c2097549
Dominick Washburn <domwashburn@gmail.com>
2015-05-03 20:16:24 -0500

    added active and disabled states for controls.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,10 +7,14 @@ $(".button--flip").on('click',
   function() {
   	if( $(this).hasClass("is-front") ) {
 		TweenLite.to($(".card__holder").find(".card"), 1.2, {rotationY:180, ease:Back.easeOut}),
-    	$(this).removeClass("is-front").addClass("is-back");
+    	$(this).removeClass("is-front").addClass("is-back"),
+    	$(".button--image").removeClass("is-active").addClass("is-disabled"),
+    	$(".button--text").removeClass("is-disabled").addClass("is-active");
 	} else if( $(this).hasClass("is-back") ) {
 		TweenLite.to($(".card__holder").find(".card"), 1.2, {rotationY:0, ease:Back.easeOut}),
-    	$(this).removeClass("is-back").addClass("is-front");
+    	$(this).removeClass("is-back").addClass("is-front"),
+    	$(".button--image").removeClass("is-disabled").addClass("is-active"),
+    	$(".button--text").removeClass("is-active").addClass("is-disabled");
 	}
   }
 );

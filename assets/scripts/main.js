ddeb8fc0d6d3565bcc20612af8128004c218f7b0
Dominick Washburn <domwashburn@gmail.com>
2015-05-03 21:05:23 -0500

    made it so that text editing is "disabled" on the back side ... since that's not technically an expressionery function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,12 +9,12 @@ $(".button--flip").on('click',
 		TweenLite.to($(".card__holder").find(".card"), 1.2, {rotationY:180, ease:Back.easeOut}),
     	$(this).removeClass("is-front").addClass("is-back"),
     	$(".button--image").removeClass("is-active").addClass("is-disabled"),
-    	$(".button--text").removeClass("is-disabled").addClass("is-active"); 
+    	$(".button--text").removeClass("is-active").addClass("is-disabled"); 
 	} else if( $(this).hasClass("is-back") ) {
 		TweenLite.to($(".card__holder").find(".card"), 1.2, {rotationY:0, ease:Back.easeOut}),
     	$(this).removeClass("is-back").addClass("is-front"),
     	$(".button--image").removeClass("is-disabled").addClass("is-active"),
-    	$(".button--text").removeClass("is-active").addClass("is-disabled");
+    	$(".button--text").removeClass("is-disabled").addClass("is-active");
 	}
   }
 );
@@ -36,7 +36,6 @@ resizeCheck = function () {
 				cardWidth = $(".card__image").width();
 				$(".card, .card__face").height(cardHeight).width(cardWidth);
 				$(".card__holder").height(cardHeight);
-	           alert(cardHeight + " --- " + cardWidth);
 	        }            
 	    }
 	});
